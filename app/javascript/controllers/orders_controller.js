7cb72f5e26458b11e1098eb7ecf6f6caeb983944
juanicolas@msn.com <juanicolas@msn.com>
2022-06-24 03:55:57 -0500

    calculadora efectivo lista!

--- app/javascript/controllers/orders_controller.js
+++ app/javascript/controllers/orders_controller.js
@@ -11,4 +11,21 @@ export default class extends Controller {
           event.preventDefault()
       }
   }
+  calculator(event) {
+      let method = event.currentTarget.value
+      if(method == 'Efectivo'){
+          document.querySelector("#calculator").hidden = false
+      }else{
+          document.querySelector("#calculator").hidden = true
+      }
+  }
+  calculate(event) {
+      console.log(event.params.original)
+      let curent = event.currentTarget.value
+      if(curent > event.params.original){
+          document.querySelector("#change").value = curent - event.params.original
+      }else{
+          document.querySelector("#change").value = 0
+      }
+  }
 }
