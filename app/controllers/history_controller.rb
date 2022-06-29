13925d3ee54be5958e4d55d49776df3e6651462a
juanicolas@msn.com <juanicolas@msn.com>
2022-06-28 22:50:29 -0500

    fila de totales completa

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,6 +1,20 @@
 class HistoryController < ApplicationController
-    def index
-      @q = Order.ransack(params[:q])
-      @pagy, @orders = pagy(@q.result(distinct: true), items: 15)
+  append_before_action do
+    unless current_user && current_user.admin?
+      flash[:alert] = 'No tiene permiso para esta acción'
+      redirect_to root_path
     end
+  end
+
+
+  def index
+      parameters = params[:q]
+      @q = Order.ransack(parameters)
+      total_list = @q.result
+      @total_value_sum = total_list.sum(:total_value)
+      @total_aditions_sum  = total_list.sum(:aditions)
+      @total_tip_sum = total_list.sum(:tip)
+      @total_final_value_sum = total_list.sum(:final_value)
+      @pagy, @orders = pagy(total_list, items: 15)
+  end
 end
