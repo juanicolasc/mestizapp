adfa4a7c7fe3fa9df712aa609d5d8b71c91d0e30
juanicolas@msn.com <juanicolas@msn.com>
2022-07-03 01:59:15 -0500

    validar unicidad de identificación y email si fueronn preveeidos

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ApplicationRecord
     validates_presence_of :name
+    validates :identification, uniqueness: true, allow_blank: true
+    validates :email, uniqueness: true, allow_blank: true
     has_many :orders
 
     def to_s
